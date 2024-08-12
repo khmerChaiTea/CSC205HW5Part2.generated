a71bfe822ddbc1fef5688a38d08099d0763012d6
pchai <prempreychhay@gmail.com>
2024-08-12 15:34:43 -0700

    Great Tutorial for banking transaction

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -13,27 +13,3 @@ account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
 Console.WriteLine(account.Balance);
 
 Console.WriteLine(account.GetAccountHistory());
-
-//// Test to ensure withdrawals do not result in a negative balance
-//try
-//{
-//    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
-//}
-//catch (InvalidOperationException e)
-//{
-//    Console.WriteLine("Exception caught trying to overdraw");
-//    Console.WriteLine(e.ToString());
-//}
-
-//// Test to ensure that accounts cannot be created with a negative initial balance
-//BankAccount invalidAccount;
-//try
-//{
-//    invalidAccount = new BankAccount("invalid", -55);
-//}
-//catch (ArgumentOutOfRangeException e)
-//{
-//    Console.WriteLine("Exception caught creating account with negative balance");
-//    Console.WriteLine(e.ToString());
-//    return;
-//}
