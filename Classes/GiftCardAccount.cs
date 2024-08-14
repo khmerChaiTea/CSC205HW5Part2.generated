b28fdb4765bbff3eba43cdb6f74cc6781450d75e
pchai <prempreychhay@gmail.com>
2024-08-13 19:53:57 -0700

    Add the following code to the Main method to test these changes for the GiftCardAccount and the InterestEarningAccount

--- Classes/GiftCardAccount.cs
+++ Classes/GiftCardAccount.cs
@@ -8,8 +8,17 @@ namespace Classes
 {
     public class GiftCardAccount : BankAccount
     {
-        public GiftCardAccount(string name, decimal initialBalance) : base(name, initialBalance)
+        private readonly decimal _monthlyDeposit = 0m;
+
+        public GiftCardAccount(string name, decimal initialBalance, decimal monthlyDeposit = 0) : base(name, initialBalance)
+            => _monthlyDeposit = monthlyDeposit;
+
+        public override void PerformMonthEndTransactions()
         {
+            if (_monthlyDeposit != 0)
+            {
+                MakeDeposit(_monthlyDeposit, DateTime.Now, "Add monthly deposit");
+            }
         }
     }
 }
